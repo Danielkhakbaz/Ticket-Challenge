9d749ca0f16830b71c973e1cc38e761741e06c79
unknown <danialkhakbaz.dk@gmail.com>
2021-03-06 17:24:06 +0330

    Add the Routing system

---
+++ ticket-challenge/src/Router.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const AppRouter = () => {
+    <Router>
+        <Switch></Switch>
+    </Router>;
+};
+
+export default AppRouter;
