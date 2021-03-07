a114d85bc6386de7ecae777e76e87953ffbcc866
unknown <danialkhakbaz.dk@gmail.com>
2021-03-07 08:35:27 +0330

    Add the StadiumDetails Page

--- ticket-challenge/src/views/pages/stadiums-details/StadiumsDetails.jsx
+++ ticket-challenge/src/views/pages/stadiums-details/StadiumsDetails.jsx
@@ -5,24 +5,110 @@ import "./StadiumsDetails.css";
 
 const StadiumsDetails = ({ match }) => {
     const id = match.params.id;
-    const [seats, setSeats] = useState([]);
+    const [seats, setSeats] = useState([
+        1,
+        0,
+        1,
+        0,
+        1,
+        0,
+        1,
+        0,
+        1,
+        0,
+        1,
+        1,
+        0,
+        1,
+        0,
+        0,
+        1,
+        0,
+        1,
+        1,
+        0,
+        1,
+        0,
+        0,
+        1,
+        0,
+        1,
+        1,
+        0,
+        1,
+        0,
+        0,
+        1,
+        0,
+        1,
+        1,
+        0,
+        1,
+        0,
+        0,
+        1,
+        0,
+        1,
+        1,
+        0,
+        1,
+        0,
+        0,
+        1,
+        0,
+        1,
+        1,
+        0,
+        1,
+        0,
+        0,
+        1,
+        0,
+        1,
+        1,
+        0,
+        1,
+        0,
+    ]);
 
-    useEffect(() => {
-        const fetchAPI = async () => {
-            console.log(seats);
-            const { data } = await axios.get(`${apiURL}/map/${id}`).then((res) => {
-                console.log(data);
-                console.log(res);
-            });
-        };
-        fetchAPI();
-    });
+    // useEffect(() => {
+    //     const fetchAPI = async () => {
+    //         console.log(seats);
+    //         const { data } = await axios.get(`${apiURL}/map/${id}`).then((res) => {
+    //             setSeats(data.data)
+    //         });
+    //     };
+    //     fetchAPI();
+    // });
+
+    const handleBooking = async () => {
+        axios.post(`${apiURL}/map/${id}/ticket`, {}).then(
+            (response) => {
+                console.log(response);
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    };
 
     return (
         <>
             <section className="stadiumDetails">
-                <h4 className="stadiumDetails__text">{id} Stadium</h4>
-
+                <div className="stadiumDetails__div">
+                    <h4 className="stadiumDetails__text">{id} Stadium</h4>
+                    <div className="stadiumDetails__seats">
+                        {seats.map((seat) => (
+                            <button
+                                className="stadiumDetails__seat"
+                                disabled={seat === 1 && "disabled"}
+                                onClick={() => handleBooking()}
+                            >
+                                {seat}
+                            </button>
+                        ))}
+                    </div>
+                </div>
             </section>
         </>
     );
--- ticket-challenge/src/views/pages/stadiums/Stadiums.jsx
+++ ticket-challenge/src/views/pages/stadiums/Stadiums.jsx
@@ -1,32 +1,55 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
-import { apiURL } from "../../../services/api/Config.json";
+// import axios from "axios";
+// import { apiURL } from "../../../services/api/Config.json";
 import "./Stadiums.css";
 
-const Stadiums = () => {
-    const [stadiums, setStadiums] = useState([]);
+const Stadiums = ({ history }) => {
+    const [stadiums, setStadiums] = useState([
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+    ]);
 
-    useEffect(() => {
-        const fetchAPI = async () => {
-            await axios.get(`${apiURL}/map`).then((data) => {
-                console.log(data);
-                setStadiums(data.data.data.map_ids);
-            });
-        };
-        fetchAPI();
-    }, []);
+    // useEffect(() => {
+    //     const fetchAPI = async () => {
+    //         const { data } = await axios.get(`${apiURL}/map`);
+    //         setStadiums(data.data.data.map_ids);
+    //     };
+    //     fetchAPI();
+    // }, []);
+
+    const handleBack = () => {
+        return history.push("/");
+    };
 
     return (
         <>
             <section className="stadium">
                 <div className="stadium__div">
                     <h4 className="stadium__text">Stadiums</h4>
-                    {stadiums.map((stadium) => (
-                        <div key={stadium}>
-                            <Link to={`/stadiums/${stadium}`}>{stadium}</Link>
-                        </div>
-                    ))}
+                    <div className="stadium__items">
+                        {stadiums.map((stadium) => (
+                            <button className="stadium__button" key={stadium}>
+                                <Link to={`/stadiums/${stadium}`}>
+                                    {stadium}
+                                </Link>
+                            </button>
+                        ))}
+                    </div>
+                    <button
+                        className="stadium__button--back"
+                        onClick={handleBack}
+                    >
+                        Go Back
+                    </button>
                 </div>
             </section>
         </>
